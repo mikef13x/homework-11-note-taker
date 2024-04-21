6e3a2a908c0cd79c13fa35dc3cf89957b9a43598
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-04-21 19:17:23 -0400

    updated push to allow transfer to other pc

--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public'));ç
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
