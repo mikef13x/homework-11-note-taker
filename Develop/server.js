1b0acbbcedac92e3311c07943a39bdb770396b4c
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-04-21 19:16:35 -0400

    tried a new way to read file, db.json updating but not pushing to server

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,9 @@
 
+const fs = require('fs')
 const express = require('express')
-const { readFromFile, readAndAppend } = require ('./helpers/fsUtils')
 const { v4: uuid } = require ("uuid");
 const database = require("./db/db.json");
-const fs = require('fs')
+
 
 const path = require ("path");
 
@@ -16,31 +16,22 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    
+    fs.readFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+app.post('/api/notes', (req, res) => {
+    const note = JSON.parse(fs.readFileSync('./db/db.json'))
+    const newNote = req.body;
+    newNote.id = uuid()
+    note.push(newNote)
+    fs.writeFileSync('./db/db.json', JSON.stringify(note))
+    res.json(note)
+})
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-app.get('/api/notes', (req, res) => {
-    
-    readFromFile(database).then((data) => res.json(JSON.parse(data)))
-});
-app.post('/api/notes', (req, res) => {
-    const { title ,text} = req.body;
-    if(req.body) {
-        const newNote = {
-            title,
-            text,
-            title_id: uuid()
-        }
-    
-     
-    
-    readAndAppend(newNote, JSON.stringify(database)); // Convert database array to a string
-    res.json('note added successfully')
-    }else {
-        res.error('error adding note')
-    }
-})
-
 
 app.listen(PORT, () => 
 console.log(`App listening at http://localhost:${PORT}`))
